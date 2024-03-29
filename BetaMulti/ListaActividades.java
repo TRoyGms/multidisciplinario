508f3b006a8483093f2b66f0f2ba3f816c8786a6
AlejandroZBarajas <falejandrozb@gmail.com>
2024-03-28 18:25:48 -0600

    [removeAt()method]

--- BetaMulti/ListaActividades.java
+++ BetaMulti/ListaActividades.java
@@ -3,17 +3,20 @@ import java.util.ArrayList;
 public class ListaActividades{
     private int size;
 
-   public ArrayList <Actividad> ListaActividades = new ArrayList<>();
+   public ArrayList <Actividad> listaActividades = new ArrayList<>();
 
     public void addActividad(Actividad actividad){
-        ListaActividades.add(actividad);
+        listaActividades.add(actividad);
         this.size++;
         }
     
     public ArrayList <Actividad> getListaActividades(){
-        return ListaActividades;
+        return listaActividades;
     }
     public int getSize(){
         return this.size;
+    }
+    public void removeAt(int index){
+        listaActividades.remove(index);
     }
 }
